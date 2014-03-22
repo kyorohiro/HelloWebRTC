5358b36b028268f9c7d397edde3b6c963953f7e8
kyorohiro <kyorohiro@gmail.com>
2014-03-23 03:07:36 +0900

    ..

--- testDSHT/util/bencode.js
+++ testDSHT/util/bencode.js
@@ -1,9 +1,16 @@
 function Bencode() {
 	this.root = {};
-	
-	this.encode() {
-		var buffer = ArrayBuffer();
-		for(i in this.root) {
+
+	this._encode(_obj) {
+		var builder = ArrayBuilder();
+		this.__encode(_obj, builder)
+		return builder;
+	}
+	this.__encode(_obj, builder) {
+		builder.appendText("d");
+		for(key in _obj) {
+			builder.appendText(""+key.length+":"+__encode(_obj[jey],builder));
 		}
+		builder.appendText("e");
 	}	
 }
